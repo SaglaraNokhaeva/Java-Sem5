df4c4b249fa6f41b955bc1b8c7050f37b812075f
Saglara <nohaevass@yandex.ru>
2023-02-04 19:32:12 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -10,10 +10,22 @@
 // Вывести данные по сотрудникам с фамилией Иванов.
 
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class task1 {
     public static void main(String[] args) {
-        
+        Map <Integer, String> db = new HashMap<>();
+        db.put(123456, "Иванов");
+        db.put(321456, "Васильев");
+        db.put(234561, "Петрова");
+        db.put(234432, "Иванов");
+        db.put(654321, "Петрова");
+        db.put(345678, "Иванов");
+
+        Scanner scan = new Scanner(System.in);
+        String sekond_name = scan.nextLine();
+        for (cell)
     }
     
 }
